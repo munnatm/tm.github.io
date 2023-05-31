2123b07c03ff82ed595089183544957fc4136e54
munnatm <135148810+munnatm@users.noreply.github.com>
2023-05-31 23:15:59 +0600

    proposal

---
+++ index.js
@@ -0,0 +1,6 @@
+document
+  .getElementById("proposal-button")
+  .addEventListener("click", function () {
+    document.getElementById("response-message").innerHTML =
+      "Congratulations! I can't wait to spend the rest of my life with you!";
+  });
